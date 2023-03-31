b4d3d4cad22224abbbbbe301addb22a7bedf56e0
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-03-31 08:34:54 -0600

    Agregar archivos de proyecto.

---
+++ MVCPersonInfo/Controllers/AddressController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPersonInfo.Models;
+
+namespace MVCPersonInfo.Controllers
+{
+    public class AddressController : Controller
+    {
+
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: AddressController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddressController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AddressModel data)
+        {
+            if  (!ModelState.IsValid) {
+                _logger.LogWarning("The user submitted invalid Address");
+            }
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ MVCPersonInfo/Models/AddressModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCPersonInfo.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        [Display(Name ="Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        [StringLength(10,MinimumLength =5)]
+        public string PostalCode { get; set; }
+    }
+}
